b5a92aaf9ae6fe802c631410821cadf9bfe3a90b
ztarc12 <ztarc12@gmail.com>
2024-07-11 14:19:09 +0900

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,129 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import cardData from "./cardData";
+
+import "./card.css"
+import logo from './logo.svg'
+
+function Card() {
+  const [cards, setCards] = useState([]);
+  const [flipCards, setFlipCards] = useState([]);
+  const [matchCards, setMatchCards] = useState([]);
+  const [gameStart, setGameStart] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(60);
+
+  //카드 섞기
+  const suffledCards = () => {
+    const shuffleCard = [...cardData, ...cardData] // 2개의 카드를 비교하기 위해 15개 데이터가 있는 cardData 두번 호출
+      .sort(()=>Math.random() - 0.5) // 카드 배열 무작위 섞기
+      .map(card => ({...card, id: Math.random()})); // 카드 별 id값 부여
+    
+    setCards(shuffleCard)
+  }
+
+  useEffect(() => {
+    suffledCards();
+  }, []);
+
+  //카드 클릭 확인
+  const cardClick = (id) => {
+    if(!gameStart) {
+      alert("'시작하기' 버튼을 클릭해주세요.")
+      return
+    }
+    if(flipCards.length < 2 && !flipCards.includes(id) && !matchCards.includes(id)) //카드가 뒤집힌 상태인지, 이미 뒤집힌 카드인지, 이미 맞춘 카드인지 확인
+    {
+      setFlipCards([...flipCards, id]); // 위 조건을 모두 통과하면 flipCards 배열에 추가
+    }
+  }
+  // 두개의 카드 비교
+  useEffect(()=>{
+    if(flipCards.length === 2) {
+      const [firstCard, secondCard] = flipCards.map(id=>
+        cards.find(card=>card.id === id));
+      if (firstCard.name === secondCard.name) {
+        setMatchCards([...matchCards, firstCard.id, secondCard.id])
+      }
+      setTimeout(()=>setFlipCards([]),1000);
+    }
+  }, [flipCards, cards])
+
+  // 타이머 설정
+  useEffect(()=>{
+    let timer
+    if(gameStart && timeLeft > 0) {
+      timer = setInterval(() => {
+        setTimeLeft(prevTime => prevTime - 1)
+      }, 1000);
+    } else if (timeLeft === 0) {
+      alert('실패! 시간 초과')
+      setGameStart(false)
+    }
+    return () => clearInterval(timer)
+  }, [gameStart, timeLeft])
+
+  // 시작하기 클릭
+  const startGame = () => {
+    setGameStart(true)
+    setTimeLeft(60)
+    setFlipCards([])
+    setMatchCards([])
+    // suffledCards()
+  }
+  
+  // 초기화 클릭
+  const resetGame = () => {
+    setGameStart(false)
+    setTimeLeft(60)
+    setFlipCards([])
+    setMatchCards([])
+    suffledCards()
+  }
+
+  // 성공 여부 확인
+  useEffect(()=>{
+    if(matchCards.length === cards.length && matchCards.length) {
+      alert('성공! 모든 카드를 맞췄습니다.')
+      setGameStart(false)
+      resetGame()
+    }
+  }, [matchCards, cards])
+
+  return (
+    <section>
+      <div style={{paddingTop:'30px'}}>
+        <h2>카드 뒤집기 게임</h2>
+        <div className='card-grid'>
+          {
+            cards.map((card)=>{
+              return(
+                <CardShuffle key={card.id} card={card} cardClick={cardClick} isFlip={gameStart ? flipCards.includes(card.id) : true} isMatch={matchCards.includes(card.id)}/>
+              )
+            })
+          }
+        </div>
+        <div className="btnBox">
+          <button className="startBtn" onClick={()=>{startGame()}}>시작하기</button>
+          <button className="resetBtn" onClick={()=>{resetGame()}}>초기화</button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+function CardShuffle({card, cardClick, isFlip, isMatch}){
+  return(
+      <div onClick={()=>{cardClick(card.id)}} className={`card ${isFlip || isMatch ? 'flipped' : ''}`}>
+        <div className="card-inner">
+          <div className="card-front">
+            <img src={logo}></img>
+          </div>
+          <div className="card-back">
+            <img src={card.img} alt={card.name}></img>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Card
---
+++ src/cardData.js
@@ -0,0 +1,19 @@
+const cardData = [
+  {id:1 , name: 'a', img:'/images/redsnail.gif'},
+  {id:2 , name: 'b', img:'/images/redrebbonpig.gif'},
+  {id:3 , name: 'c', img:'/images/slime.gif'},
+  {id:4 , name: 'd', img:'/images/octopus.gif'},
+  {id:5 , name: 'e', img:'/images/orangemushroom.gif'},
+  {id:6 , name: 'f', img:'/images/hornmushroom.gif'},
+  {id:7 , name: 'g', img:'/images/stump.gif'},
+  {id:8 , name: 'h', img:'/images/legator.gif'},
+  // {id:9 , name: 'i', img:'/img/redsnail.webp'},
+  // {id:10 , name: 'j', img:'/img/redsnail.webp'},
+  // {id:11 , name: 'k', img:'/img/redsnail.webp'},
+  // {id:12 , name: 'l', img:'/img/redsnail.webp'},
+  // {id:13 , name: 'm', img:'/img/redsnail.webp'},
+  // {id:14 , name: 'n', img:'/img/redsnail.webp'},
+  // {id:15 , name: 'o', img:'/img/redsnail.webp'},
+]
+
+export default cardData;
